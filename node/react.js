4c1b4ccd76ffe05b8c16ad3b364a64cd1999b7ee
selvakumar22400 <78866668+selvakumar22400@users.noreply.github.com>
2021-09-23 22:01:21 +0530

    Create react.js

---
+++ node/react.js
@@ -0,0 +1,17 @@
+install nodejs 
+Incommand prompt
+  node --version
+  npm --version
+create react app
+  npx create-react-app my-app(<-userdefine name)
+  //happy hhacking massage is displayed
+  cd my-app
+  npm start
+  //app will be visibile in defulat browser
+open the new cmd prompt
+  cd my-app
+  code .//code will open on vs code
+  
+
+  
+  
